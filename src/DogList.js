7c2e7256703f7cd6dfad7d602ce5cea0831744b6
cdsalty <soltiscd@gmail.com>
2020-01-09 15:29:53 -0500

    DogList component created at /dogs

---
+++ src/DogList.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './DogList.css';
+
+class DogList extends Component {
+  render() {
+    return (
+      <div className="DogList">
+        <h2 className="display-2 text-center">Dog List</h2>
+        <div className="container">
+          <div className="row">
+            {this.props.dogs.map((dog) => (
+              <div className="Dog col-md-6 col-lg-4 text-center">
+                <img src={dog.src} alt={dog.name} />
+                <h4>{dog.name}</h4>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default DogList;
+
+/*
+In the return, the idea is to hardcode some of the html and have the dog columns dynamically inserted.
+Need to create three columns on one row. Each column to hold a dog
+  ->  <div className = "col-4 text-center"></div>
+*/
